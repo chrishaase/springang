037e1c03ad3a9df979fa4a65d9f946b7d6e1a9fe
chaase <haase.mail@web.de>
2020-02-16 11:04:40 +0100

    update mongo etc

--- src/main/java/com/example/demo/config/SimpleMongoConfig.java
+++ src/main/java/com/example/demo/config/SimpleMongoConfig.java
@@ -2,13 +2,36 @@ package com.example.demo.config;
 
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 @Configuration
 public class SimpleMongoConfig {
 
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(new MongoClientURI(env.getProperty("spring.data.mongodb.uri")));
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+
+        return mongoTemplate;
+
+    }
+
+
+    /**
     @Bean
     public MongoClient mongo() {
         return new MongoClient("localhost");
@@ -18,4 +41,5 @@ public class SimpleMongoConfig {
     public MongoTemplate mongoTemplate() throws Exception {
         return new MongoTemplate(mongo(), "test");
     }
+    **/
 }
