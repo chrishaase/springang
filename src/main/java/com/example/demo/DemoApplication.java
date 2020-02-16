cf25ded709b0230d10ee3a8a085f613d0a282aef
chaase <haase.mail@web.de>
2020-02-16 16:48:51 +0100

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,7 +22,7 @@ public class DemoApplication implements ErrorController {
 
 	@RequestMapping(value = PATH)
 	public String error() {
-		return "redirect:/";
+		return "redirect:https://spring-268410.appspot.com/";
 	}
 
 	@Override
